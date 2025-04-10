916c3a936f090104af9073aff9887ec9cc8dfe63
Pedro Henrique Ribeiro <pedroita2002ht@gmail.com>
2025-04-10 20:24:40 -0300

    Update ex0056.py

--- ex0056.py
+++ ex0056.py
@@ -6,6 +6,16 @@ for c in range(1,5):
     sexo = str(input('Sexo [M/F]:'))
     somaidade += idade
     if sexo in 'Mm':
+        homemvelho = nome
+        maioridadehomem = idade
+    if idade > maioridadehomem:
+        maioridadehomem = idade
+        homemvelho = nome
+     if sexo in 'Ff':
+         if idade < 20:
+             mulhermenos20 += 1
             
 média = somaidade / 4
 print(f'A média de idade do grupo é {média}')
+print(f'O nome do Homem mais velho é {homemvelho} com {maioridadehomem} anos')
+print('O total de mulheres com menos de 20 anos é {mulhermenos20}')      
